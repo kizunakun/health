00913e610ec6eb5dc16c39d7f068c46cbcf238d5
Fathan Muharrik <fathanmuharrik2003@gmail.com>
2024-06-10 11:39:10 +0700

    ganti flow debug

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace health
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Quest1());
+            Application.Run(new Beranda());
         }
     }
 }
