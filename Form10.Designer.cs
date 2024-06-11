84a548533cc69c67d21b31935f91d28a15bc0525
tiayaaa <tiamarcelin03@gmail.com>
2024-06-11 12:55:39 +0700

    pelajari tentang kami

--- Form10.Designer.cs
+++ Form10.Designer.cs
@@ -28,41 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            flowLayoutPanel1 = new FlowLayoutPanel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pelajari));
             label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
             SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            flowLayoutPanel1.BackColor = Color.LightSkyBlue;
-            flowLayoutPanel1.Dock = DockStyle.Top;
-            flowLayoutPanel1.Location = new Point(0, 0);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(800, 53);
-            flowLayoutPanel1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.Transparent;
-            label1.Location = new Point(351, 175);
+            label1.BackColor = Color.FromArgb(255, 192, 192);
+            label1.Location = new Point(200, 19);
             label1.Name = "label1";
-            label1.Size = new Size(307, 30);
-            label1.TabIndex = 1;
-            label1.Text = "\"Memahami Kesehatan Mental\"";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Size = new Size(501, 32);
+            label1.TabIndex = 0;
+            label1.Text = "\"MEMAHAMI TENTANG KESEHATAN MENTAL\"";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(275, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 32);
+            label2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Lavender;
+            textBox1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(37, 70);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(837, 112);
+            textBox1.TabIndex = 3;
+            textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Lavender;
+            textBox2.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(37, 213);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(422, 358);
+            textBox2.TabIndex = 4;
+            textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.Lavender;
+            textBox3.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(465, 213);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(409, 358);
+            textBox3.TabIndex = 5;
+            textBox3.Text = resources.GetString("textBox3.Text");
             // 
             // Pelajari
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.Mental_Health_Background;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(900, 583);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(flowLayoutPanel1);
+            Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(6, 6, 6, 6);
             Name = "Pelajari";
             Text = "Form10";
             Load += Form10_Load;
@@ -72,7 +112,10 @@
 
         #endregion
 
-        private FlowLayoutPanel flowLayoutPanel1;
         private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
